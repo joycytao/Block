201a80777bfdf382dd97f2da1fefd5d58910ffe0
CHIEH-YU TAO <joy.tao@6pm-studio.com>
2022-06-10 21:51:49 -0500

    update the protocol access level for unit test

--- Square's Mobile Project/UI/UIProtocols.swift
+++ Square's Mobile Project/UI/UIProtocols.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-protocol Load {
+public protocol Load {
     
     func willStart()
     func didFinish(with result: [Employee])
@@ -28,4 +28,45 @@ protocol ResultSwitchable {
     
     func showList(with result: Result<[Employee], Error>)
     func showEmptyState()
+}
+
+public protocol FetchEmployee {
+    func getEmployee(_ type: LoadType)
+}
+
+extension FetchEmployee where Self: RootViewModel {
+    
+    public func getEmployee(_ type: LoadType) {
+        
+        loader?.didStart()
+        
+        let client = HTTPClient(session: .shared)
+        
+        var request: EmployeeRequest<EmployeeResponse>
+        switch type {
+        case .employee:
+            request = EmployeeRequest<EmployeeResponse>()
+        case .empty:
+            request = EmployeeRequest<EmployeeResponse>(url: URL(string: "https://s3.amazonaws.com/sq-mobile-interview/employees_empty.json")!, method: .GET, contentType: .json)
+        case .malformed:
+            request = EmployeeRequest<EmployeeResponse>(url: URL(string: "https://s3.amazonaws.com/sq-mobile-interview/employees_malformed.json")!, method: .GET, contentType: .json)
+        }
+        
+                
+        client.send(request) { [weak self] res in
+            
+            guard let wself = self else { return }
+            
+            switch res {
+            case .success(let value):
+                if value.employees.count > 0 {
+                    wself.datasource = value.employees
+                }
+                wself.loader?.didFinish(with: value.employees)
+                
+            case .failure(let error):
+                wself.loader?.didFail(with: error)
+            }
+        }
+    }
 }
